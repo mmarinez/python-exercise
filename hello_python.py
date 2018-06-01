1545c593b7db594bdee7ccb9667d7488c80140f6
Manuel Marinez <marinezdev@gmail.com>
2018-06-01 05:21:00 -0400

    hello app branch

--- hello_python.py
+++ hello_python.py
@@ -1,4 +1,3 @@
-def make_tshirt(tshirt_message="I love python",tshirt_size = "Medium"):
-    print("Your tshirt size is: " + tshirt_size + "\n" + "Your tshirt message is: " + tshirt_message)
-
-make_tshirt()    
+print("-------------------------------")
+print("           HELLO APP")
+print("-------------------------------")
